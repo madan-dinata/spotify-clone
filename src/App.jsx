8ca29e17a6fdfe9b26b06a2b863489d8007a0cf2
madandinata <madanmr48@gmail.com>
2023-07-31 13:40:09 +0700

    added daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Fragment } from "react"
 import "./App.css"
 
 export default function App() {
-  return <h1 className="text-center text-3xl font-bold underline">Hello World!</h1>
+  return (
+    <Fragment>
+      <h1 className="text-center text-3xl font-bold underline">Hello World!</h1>
+      <button className="btn btn-primary">Primary</button>
+    </Fragment>
+  )
 }
