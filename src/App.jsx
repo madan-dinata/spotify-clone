d3d680660db93833d3a93b8d4e50ab06c9bef454
madandinata <madanmr48@gmail.com>
2023-08-04 11:05:53 +0700

    added get recommendation and playlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios"
 import Login from "./pages/Login"
 import useAuth from "./hooks/useAuth"
 import { useEffect } from "react"
+import songImage from "/images/song.png"
 
 export default function App() {
   const { token, login, logout } = useAuth()
@@ -14,15 +15,15 @@ export default function App() {
   const [searchQuery, setSearchQuery] = useState("")
   const [searchResults, setSearchResults] = useState([])
 
-  const handleSearch = () => {
-    axios
+  const handleSearch = async () => {
+    return await axios
       .get(`${SEARCH}?q=${searchQuery}&type=track`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
       })
-      .then((response) => {
-        setSearchResults(response.data.tracks.items)
+      .then((res) => {
+        setSearchResults(res.data.tracks.items)
       })
       .catch((error) => {
         console.error("Error searching tracks:", error)
@@ -41,24 +42,106 @@ export default function App() {
     }
   }, [searchQuery])
 
+  // get recommendation
+  // eslint-disable-next-line no-unused-vars
+  const [recommendation, setRecommendation] = useState([])
+
+  const handleRecom = async (token) => {
+    // function masih harus karena terlambat ngeget token jadi kebutu error dulu
+    return await axios
+      .get("https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setRecommendation(res.data)
+      })
+      .catch((err) => {
+        console.error("Error get recommendation", err)
+      })
+  }
+
+  useEffect(() => {
+    handleRecom(localStorage.getItem("accessToken"))
+  }, [token])
+
+  // playlist
+  const [playlist, setPlaylist] = useState([])
+
+  const getPlaylist = async (token) => {
+    return await axios
+      .get("https://api.spotify.com/v1/me/playlists", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setPlaylist(res.data)
+      })
+      .catch((err) => {
+        console.error("Error get playlist", err)
+      })
+  }
+
+  const handlePlaylist = () => {
+    console.log(playlist)
+  }
+
+  useEffect(() => {
+    getPlaylist(localStorage.getItem("accessToken"))
+  }, [token])
+
   return (
     <>
       {!localStorage.getItem("accessToken") ? (
         <Login token={token} handleLogin={login} />
       ) : (
         <div className="grid grid-cols-12 gap-3 py-1 px-1">
-          <div className="py-1 h-screen fixed w-[25rem]">
+          <div className="py-1 h-[99vh] fixed w-[25rem]">
             <div className="bg-[#121212] rounded-xl py-4 px-2 mb-2">
               <h2 className="text-md font-semibold mx-4 my-2 cursor-pointer text-white">Home</h2>
               <h2 className="text-md font-semibold mx-4 my-2 cursor-pointer">Search</h2>
             </div>
-            <div className="bg-[#121212] rounded-xl py-4 px-2 h-screen">
-              <h2 className="text-md font-semibold mb-4 mx-4 cursor-pointer">Your Library</h2>
-              <div className="px-4 bg-[#242424] rounded-lg py-3 text-white">
-                <p className="font-semibold mb-2">Create your first playlist</p>
-                <p className="mb-2">It&apos;s easy, we&apos;ll help you</p>
-                <button className="rounded-2xl bg-white hover:bg-[#f1f1f1] text-black px-5 py-1 mt-5 text-sm">Create playlist</button>
+            <div className="bg-[#121212] py-4 px-2 max-h-full">
+              <div className="flex justify-between">
+                <h2 className="text-md font-semibold mb-4 mx-4 cursor-pointer">Your Library</h2>
+                <span className="btn" onClick={handlePlaylist}>
+                  +
+                </span>
               </div>
+              {!playlist ? (
+                <>
+                  <div className="px-4 bg-[#242424] rounded-lg py-3 text-white">
+                    <p className="font-semibold mb-2" onClick={handlePlaylist}>
+                      Create your first playlist
+                    </p>
+                    <p className="mb-2">It&apos;s easy, we&apos;ll help you</p>
+                    <button className="rounded-2xl bg-white hover:bg-[#f1f1f1] text-black px-5 py-1 mt-5 text-sm">Create playlist</button>
+                  </div>
+                </>
+              ) : (
+                <ul>
+                  {playlist.items.map((item) => (
+                    <li key={item.id} className="hover:bg-[#242424] cursor-pointer mt-1 p-3 rounded-md">
+                      <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                          <div className="mask w-12 h-12">
+                            <img src={item.images.length > 0 ? item.images[0].url : songImage} alt={item.name} className="bg-[#282828]" />
+                          </div>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="font-bold whitespace-no-wrap truncate w-64">{item.name}</div>
+                          <div className="flex text-sm opacity-50">
+                            {item.type} - {item.owner.display_name}
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           </div>
 
@@ -75,53 +158,113 @@ export default function App() {
                 </div>
               </div>
               <br />
-              <h2 className="text-xl font-semibold mb-4">Song List</h2>
-              <table className="table w-full">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Title</th>
-                    <th>Album</th>
-                    <th>Duration</th>
-                  </tr>
-                </thead>
-                <tbody className="cursor-pointer">
-                  {searchResults.map((track, i) => (
-                    <tr key={track.id} className="hover:bg-[#121212]">
-                      <td>{i + 1}</td>
-                      <td>
-                        <div className="flex items-center space-x-3">
-                          <div className="avatar">
-                            <div className="mask w-12 h-12">
-                              <img src={track.album.images[2].url} alt={track.album.name} />
+              {/* search */}
+              {searchQuery && (
+                <>
+                  <h2 className="text-xl font-semibold mb-4">Song List</h2>
+                  <table className="table w-full">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Album</th>
+                        <th>Duration</th>
+                      </tr>
+                    </thead>
+                    <tbody className="cursor-pointer">
+                      {searchResults?.map((track, i) => (
+                        <tr key={track.id} className="hover:bg-[#121212]">
+                          <td>{i + 1}</td>
+                          <td>
+                            <div className="flex items-center space-x-3">
+                              <div className="avatar">
+                                <div className="mask w-12 h-12">
+                                  <img src={track.album.images[2].url} alt={track.album.name} />
+                                </div>
+                              </div>
+                              <div>
+                                <div className="font-bold">{track.name}</div>
+                                <div className="text-sm opacity-50">{track.artists.map((artist) => artist.name).join(", ")}</div>
+                              </div>
                             </div>
-                          </div>
-                          <div>
-                            <div className="font-bold">{track.name}</div>
-                            <div className="text-sm opacity-50">{track.artists.map((artist) => artist.name).join(", ")}</div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>{track.album.name}</td>
-                      <td>
-                        {(() => {
-                          const menit = Math.floor(track.duration_ms / 60000)
-                          const detikSisa = Math.floor((track.duration_ms % 60000) / 1000)
-                          return `${menit}:${detikSisa.toString().padStart(2, "0")}`
-                        })()}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <th>No</th>
-                    <th>Title</th>
-                    <th>Album</th>
-                    <th>Duration</th>
-                  </tr>
-                </tfoot>
-              </table>
+                          </td>
+                          <td>{track.album.name}</td>
+                          <td>
+                            {(() => {
+                              const menit = Math.floor(track.duration_ms / 60000)
+                              const detikSisa = Math.floor((track.duration_ms % 60000) / 1000)
+                              return `${menit}:${detikSisa.toString().padStart(2, "0")}`
+                            })()}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Album</th>
+                        <th>Duration</th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </>
+              )}
+
+              {/* recomemndation */}
+              {!searchQuery && (
+                <>
+                  <h2 className="text-xl font-semibold mb-4">Recommendation List</h2>
+                  <table className="table w-full">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Album</th>
+                        <th>Duration</th>
+                      </tr>
+                    </thead>
+                    <tbody className="cursor-pointer">
+                      {recommendation?.tracks.map((track, i) => (
+                        <tr key={track.id} className="hover:bg-[#121212]">
+                          <td>{i + 1}</td>
+                          <td>
+                            <div className="flex items-center space-x-3">
+                              <div className="avatar">
+                                <div className="mask w-12 h-12">
+                                  <img src={track.album.images[2].url} alt={track.album.name} />
+                                </div>
+                              </div>
+                              <div>
+                                <div className="font-bold">{track.name}</div>
+                                <div className="text-sm opacity-50">{track.artists.map((artist) => artist.name).join(", ")}</div>
+                              </div>
+                            </div>
+                          </td>
+                          <td>{track.album.name}</td>
+                          <td>
+                            {(() => {
+                              const menit = Math.floor(track.duration_ms / 60000)
+                              const detikSisa = Math.floor((track.duration_ms % 60000) / 1000)
+                              return `${menit}:${detikSisa.toString().padStart(2, "0")}`
+                            })()}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Album</th>
+                        <th>Duration</th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </>
+              )}
+
+              {/* footer */}
               <footer className="border-t border-gray-500 h-fit pt-8 py-16 mt-16">
                 <p className="text-end">&copy; 2023 Spotify Clone</p>
               </footer>
